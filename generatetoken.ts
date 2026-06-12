cab4b02a93e62be985e02dbc209233f35faeee54
Prath2244 <163981164+Prath2244@users.noreply.github.com>
2026-06-12 17:06:32 +0530

    Add files via upload
    Placeholder for generating a login token.

---
+++ generatetoken.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (
+  userId: string,
+  role: string
+): string => {
+  return jwt.sign(
+    {
+      id: userId,
+      role,
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
+
+export default generateToken;
