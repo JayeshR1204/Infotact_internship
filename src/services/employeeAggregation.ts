812b916ed6f9c27e03a8def0a8e231cd497e930f
Prathmesh M <163981164+Prath2244@users.noreply.github.com>
2026-06-25 07:45:27 +0530

    Add files via upload

---
+++ src/services/employeeAggregation.ts
@@ -0,0 +1,36 @@
+import Employee from "../models/Employee";
+
+export const getEmployeeDetails = async () => {
+
+    return Employee.aggregate([
+
+        {
+            $lookup: {
+                from: "departments",
+                localField: "department",
+                foreignField: "_id",
+                as: "department"
+            }
+        },
+
+        {
+            $lookup: {
+                from: "payrolls",
+                localField: "_id",
+                foreignField: "employee",
+                as: "payroll"
+            }
+        },
+
+        {
+            $lookup: {
+                from: "users",
+                localField: "user",
+                foreignField: "_id",
+                as: "user"
+            }
+        }
+
+    ]);
+
+};
