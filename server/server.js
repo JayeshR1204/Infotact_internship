17401c1da5a29bc53852ffca459ce1dd9772673e
Jayesh Raktade <154681273+JayeshR1204@users.noreply.github.com>
2026-06-25 23:51:00 +0530

    Add leaveRoutes to server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/leaves",require("./routes/leaveRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
