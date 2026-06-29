6502bab3d72ba6f452b429acbc72c8935b4e9ec8
Jayesh Raktade <154681273+JayeshR1204@users.noreply.github.com>
2026-06-29 23:28:59 +0530

    Add helmet and express-rate-limit middleware

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const connectDB = require("./config/db");
 
 dotenv.config();
